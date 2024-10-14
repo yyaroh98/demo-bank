82744942adf3e62dc4619c27f824a07111c93772
Eugene Yarosh <yaroshzhenya_98@mail.ru>
2024-10-14 22:48:26 +0300

    refactor useaccount class

--- scr/UseAccount.java
+++ scr/UseAccount.java
@@ -1,22 +1,23 @@
-import static java.lang.System.out;
+package scr;
 
 public class UseAccount {
     public static void main(String[] args) {
         Account myAccount = new Account();
         Account yourAccount = new Account();
-        myAccount.setName("Барри");
-        myAccount.setAddress("222 Inner Lane");
-        myAccount.setBalance(22.02);
-        yourAccount.setName("Джейн");
-        yourAccount.setAddress("121 Outer Street");
-        yourAccount.setBalance(55.63);
+        myAccount.name("Барри");
+
+        myAccount.address("222 Inner Lane");
+        myAccount.balance(22.02);
+        yourAccount.name("Джейн");
+        yourAccount.address("121 Outer Street");
+        yourAccount.balance(55.63);
         myAccount.display();
         System.out.print("плюс $ ");
 
         System.out.print(myAccount.getInterest(5.00));
 
         System.out.println(" дохода ");
-        System.out.println("-----------------");
+
         yourAccount.display();
         double yourInterestRate = 7.00;
         System.out.print("плюс $ ");
